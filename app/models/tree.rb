3021b807b3e1c5acb1180b3bfb6639c4960f68dd
Dennis Wang <dwang0816@gmail.com>
2019-10-16 11:09:55 -0400

    after popup

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,2 +1,9 @@
 class Tree < ApplicationRecord
+    # def self.batman
+    #     self.select()
+        
+    # end
 end
+
+
+# allTrees.filter(tree => (parseFloat(tree.longitude) < this.state.viewport["longitude"]+maxDifference && parseFloat(tree.longitude) > this.state.viewport["longitude"]-maxDifference) && (parseFloat(tree.latitude )< this.state.viewport["latitude"]+maxDifference && parseFloat(tree.latitude) > this.state.viewport["latitude"]-maxDifference))
